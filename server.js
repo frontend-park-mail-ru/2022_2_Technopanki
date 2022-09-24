2560a4009e2029f29ef72c172480d7a0667c40b2
Vladislav <sdvlad75@gmail.com>
2022-09-24 19:19:59 +0300

    added auth.html + added fixes in auth_signup.css

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
@@ -7,9 +6,19 @@ const app = express();
 const PORT = 9000
 
 app.use("/static", express.static(path.join(__dirname, 'static')));
+app.use("/img", express.static(path.join(__dirname, 'static/img')))
+app.use("/js", express.static(path.join(__dirname, "static/js")))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/signup.html'));
 });
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, `/signup.html`));
+});
+
+app.get('/auth', (req, res) => {
+    res.sendFile(path.join(__dirname, '/auth.html'))
+})
 
 app.listen(PORT);
---
+++ static/js/index.js
@@ -0,0 +1,55 @@
+const applicant = document.getElementById("applicant_toggle")
+const employer = document.getElementById("employer_toggle")
+
+const removeActive = (element) => {
+    element.classList.remove("toggle__active")
+    const icon = element.getElementsByClassName("toggle__element-icon")
+    icon[0].classList.remove("toggle__element-icon_active")
+    icon[0].classList.add("toggle__element-icon_disabled")
+}
+
+const addActive = (element) => {
+    element.classList.add("toggle__active")
+    const icon = element.getElementsByClassName("toggle__element-icon")
+    icon[0].classList.add("toggle__element-icon_active")
+    icon[0].classList.remove("toggle__element-icon_disabled")
+}
+
+applicant.addEventListener("click", () => {
+    removeActive(employer)
+    addActive(applicant)
+})
+
+employer.addEventListener("click", () => {
+    removeActive(applicant)
+    addActive(employer)
+})
+
+const form = document.getElementById("signup_form")
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const data = new FormData(e.target)
+    const values = Object.fromEntries(data.entries())
+
+    resBody = {
+        email: values.email,
+        password: values.password,
+        name: values.name,
+        surname: values.surname
+    }
+
+    const response = await fetch("/employers", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(resBody),
+    });
+
+    response.json().then(data => {
+        const dataElem = document.getElementById("data")
+        dataElem.innerText = JSON.stringify(data)
+    })
+})
