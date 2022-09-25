01081161aaf9d7440d1542d94b819ccc28aa63ef
Vladislav <sdvlad75@gmail.com>
2022-09-25 19:22:39 +0300

    added semicolons to server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 9000
+const PORT = 9000;
 
-app.use("/static", express.static(path.join(__dirname, 'static')));
-app.use("/img", express.static(path.join(__dirname, 'static/img')))
-app.use("/js", express.static(path.join(__dirname, "static/js")))
+app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/img', express.static(path.join(__dirname, 'static/img')));
+app.use('/js', express.static(path.join(__dirname, 'static/js')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/signup.html'));
@@ -18,7 +18,7 @@ app.get('/signup', (req, res) => {
 });
 
 app.get('/auth', (req, res) => {
-    res.sendFile(path.join(__dirname, '/auth.html'))
-})
+    res.sendFile(path.join(__dirname, '/auth.html'));
+});
 
 app.listen(PORT);
