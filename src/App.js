d978dff61cd506c5027c8b27fef6e3dd364268e3
Vladislav <sdvlad75@gmail.com>
2022-10-05 01:49:29 +0300

    fixed vacancy card style

--- src/App.js
+++ src/App.js
@@ -9,20 +9,10 @@ Router.addRoutePath('/', Main);
 Router.addRoutePath('/signup', SignUp);
 Router.addRoutePath('/signin', SIgnIn);
 Router.addRoutePath('/main', MainPage);
-Router.addRoutePath('/vacancies', Vacancies)
+Router.addRoutePath('/vacancies', Vacancies);
 
 window.addEventListener('popstate', () => {
     Router.render(location.pathname, true);
 });
-
-const getVacancies = async () => {
-    const response = await fetch("http://localhost:8080/api/vacancy", {
-        method: 'GET',
-        credentials: 'include',
-        mode: 'no-cors'
-    }).json().then(data => console.log(data))
-}
-
-getVacancies()
 
 Router.render(location.pathname);
--- src/components/Vacancy.js
+++ src/components/Vacancy.js
@@ -12,70 +12,82 @@ export default class Vacancy extends Component {
             'div',
             {
                 key: 'single vacancy',
-                className: "vacancy__cards-single",
+                className: 'vacancy__cards-single',
             },
+            createComponent(Link, {
+                key: 'company logo',
+                to: '/',
+                alt: 'company icon',
+                value: createText('img', {
+                    key: 'company',
+                    src: 'img/vk_logo.svg',
+                }),
+            }),
             createElement(
                 'div',
                 {
-                    key: 'vacancy body',
-                    className: 'vacancy__body'
+                    key: 'header and description',
+                    className: 'vacancy__metadata',
                 },
-                createComponent(Link, {
-                    key: 'company logo',
-                    to: '/',
-                    alt: 'company icon',
-                    value: createText('img', { key: 'company', src: 'img/vk_logo.svg' }),
-                }),
                 createElement(
                     'div',
                     {
-                        key: 'header and description',
-                        className: 'vacancy__metadata',
+                        key: 'header',
+                        className: 'vacancy__header',
                     },
+                    createComponent(Link, {
+                        key: 'vacancy name',
+                        className: 'vacancy__name',
+                        value: createText(
+                            'p',
+                            null,
+                            'Middle фронтэнд-разработчик',
+                        ),
+                    }),
                     createElement(
                         'div',
                         {
-                            key: 'header',
-                            className: 'vacancy__header'
+                            key: 'company metadata',
+                            className: 'company__metadata',
                         },
                         createComponent(Link, {
-                            key: 'vacancy name',
-                            className: 'vacancy__name',
-                            value: createText('p', null, "Middle фронтэнд-разработчик")
-                            }),
-                        createElement(
-                            'div',
-                            {
-                                key: 'company metadata',
-                                className: 'company__metadata',
-                            },
-                            createComponent(Link, {
-                                key: 'company name',
-                                to: '/',
-                                className: 'company__name',
-                                value: createText('p', null, "VK")
-                            }),
-                            createText('p', { key: 'company location' }, 'в Москве'))),
-
-                    createText('p',
-                        {
-                            key: 'vacancy description',
-                            className: 'vacancy__description'
-                        },
-                        'some description text'),
-                    createText('p',
-                        {
-                            key: 'publication date',
-                            className: 'vacancy__date'
-                        },
-                        'September 17, 2022 • 14:30')),
-                createText('h5',
+                            key: 'company name',
+                            to: '/',
+                            className: 'company__name',
+                            value: createText('p', null, 'VK'),
+                        }),
+                        createText(
+                            'p',
+                            { key: 'company location' },
+                            'в Москве',
+                        ),
+                    ),
+                ),
+                createText(
+                    'p',
                     {
-                        key: 'vacancy salary',
-                        className: 'vacancy__salary'
+                        key: 'vacancy description',
+                        className: 'vacancy__description',
                     },
-                    '100₽ в месяц')
-            )
-        )
+                    'some description text',
+                ),
+                createText(
+                    'p',
+                    {
+                        key: 'publication date',
+                        className: 'vacancy__date',
+                    },
+                    'September 17, 2022 • 14:30',
+                ),
+            ),
+            createText(
+                'h5',
+                {
+                    key: 'vacancy salary',
+                    className: 'vacancy__salary',
+                },
+                '100₽ в месяц',
+            ),
+        );
     }
 }
