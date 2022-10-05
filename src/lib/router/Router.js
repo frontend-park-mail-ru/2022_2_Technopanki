ecb6e09a4f2e37e5a6122770db459bd615434b9e
Vladislav <sdvlad75@gmail.com>
2022-10-05 12:42:07 +0300

    changed Router to Map() and added JSdoc

--- src/lib/router/Router.js
+++ src/lib/router/Router.js
@@ -1,19 +1,21 @@
 import { renderDOM } from '../core/renderDOM.js';
 
+/**
+ * Basic Router class
+ * @type {{routePathMap: Map<string, Component>, root: HTMLElement, addRoutePath(string, Component): void, render(string, bool): void}}
+ */
 export const Router = {
     root: document.getElementById('root'),
-    routePathMap: {},
+    routePathMap: new Map(),
 
     addRoutePath(path, component) {
-        this.routePathMap[path] = {
-            component: component,
-        };
+        this.routePathMap.set(path, component);
     },
 
     render(path, goBack) {
         if (!goBack) {
             window.history.pushState(null, null, path);
         }
-        renderDOM(this.root, this.routePathMap[path].component);
+        renderDOM(this.root, this.routePathMap.get(path));
     },
 };
