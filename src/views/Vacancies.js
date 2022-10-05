fec16c4f2f7e0ceded7fd88704bcc73cfb2c7490
sonichka-s <stnchnk0@gmail.com>
2022-10-05 19:40:57 +0300

    add footer

--- src/views/Vacancies.js
+++ src/views/Vacancies.js
@@ -5,6 +5,7 @@ import {
     createText,
 } from '../lib/core/VDOM/VDOMElement.js';
 import Header from '../components/Header.js';
+import Footer from '../components/Footer.js';
 import Vacancy from '../components/Vacancy.js';
 import { getVacanciesFromServer } from '../services/network/handlers/vacanciesHandler.js';
 
@@ -57,6 +58,7 @@ export default class Vacancies extends Component {
                     }),
                 ),
             ),
+            createComponent(Footer, { key: 'footer', }),
         );
     }
 }
