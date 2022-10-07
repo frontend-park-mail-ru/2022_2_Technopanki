7fbc1afeb2ac96f2faa3442067a219905d384eea
sonichka-s <stnchnk0@gmail.com>
2022-10-07 18:31:22 +0300

    fix logo size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,8 @@ export default class Header extends Component {
                     to: '/',
                     value: createElement('img', {
                         alt: 'logo svg',
-                        src: 'img/logo.svg'
+                        src: 'img/logo.svg',
+                        height: 32
                     })
                 },
             ),
