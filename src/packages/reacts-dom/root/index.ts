8da2aa6a8b3d50dbed2ef3a9945b29cdf0c10ff4
Vladislav <sdvlad75@gmail.com>
2022-10-19 23:31:53 +0300

    JSDoc(reacts library): added JSDoc in root lib

--- src/packages/reacts-dom/root/index.ts
+++ src/packages/reacts-dom/root/index.ts
@@ -1,17 +1,28 @@
 import { VNodeType } from '../../shared/common';
 import { renderNode } from '../render/renderNode';
 
+/**
+ * RootType object interface
+ */
 export interface RootType {
     render(node: VNodeType): void;
     unmount(): void;
 }
 
+/**
+ * The class that is responsible for rendering
+ * the tree root of the virtual tree in DOM
+ */
 class Root implements RootType {
     root: HTMLElement;
     constructor(root: HTMLElement) {
         this.root = root;
     }
 
+    /**
+     * Render node in root DOM node
+     * @param node
+     */
     render(node: VNodeType): void {
         renderNode(this.root, node);
     }
@@ -19,6 +30,10 @@ class Root implements RootType {
     unmount() {}
 }
 
+/**
+ * Creates Root instance
+ * @param root
+ */
 export function createRoot(root: HTMLElement | null): RootType {
     if (!root) {
         throw new Error(`Root element: ${root}`);
