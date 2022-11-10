4619bbd6cd279ed5685dadb1c7870219b779831e
Vladislav <sdvlad75@gmail.com>
2022-11-10 06:01:47 +0300

    temp delete time

--- src/components/UI-kit/resumeList/ResumeListItem.tsx
+++ src/components/UI-kit/resumeList/ResumeListItem.tsx
@@ -98,24 +98,7 @@ export default class ResumeListItem extends Component<ResumeListItemPropsType> {
                 <div
                     key={'time'}
                     className={'col-0 row align-items-center col-md-4'}
-                >
-                    <p>
-                        {`${
-                            this.props.timeWhenCreated[8] === '0'
-                                ? this.props.timeWhenCreated.slice(9, 10)
-                                : this.props.timeWhenCreated.slice(8, 10)
-                        } ${
-                            this.months.find(
-                                m =>
-                                    m.date ===
-                                    this.props.timeWhenCreated.slice(5, 7),
-                            ).name
-                        } ${this.props.timeWhenCreated.slice(
-                            0,
-                            4,
-                        )} • ${this.props.timeWhenCreated.slice(11, 16)}`}
-                    </p>
-                </div>
+                ></div>
                 <div
                     key={'button'}
                     className={
