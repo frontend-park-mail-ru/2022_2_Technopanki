506feef2904971f79bb88bd2cf4e21c6c6756aa4
Vladislav <sdvlad75@gmail.com>
2022-11-10 14:23:32 +0300

    fix(UI-kit): centered footer

--- src/components/UI-kit/footer/Footer.tsx
+++ src/components/UI-kit/footer/Footer.tsx
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <footer
-                className={`flex mt-40 py-24 row align-items-center justify-content-center`}
+                className={`flex w-100 mt-40 py-24 row align-items-center justify-content-center`}
             >
                 <JobflowLogo />
             </footer>
