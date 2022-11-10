45a3baa8f3c36b76e8dc1c088d73917c3fc0c474
Vladislav <sdvlad75@gmail.com>
2022-11-10 14:55:30 +0300

    test

--- src/components/sidebars/VacancySideBar.tsx
+++ src/components/sidebars/VacancySideBar.tsx
@@ -20,97 +20,67 @@ class VacancySideBar extends Component<{
     skills: string[];
 }> {
     render() {
-        return (
-            <SideBar
-                content={[
-                    {
-                        header: 'Заработная плата',
-                        inside: (
-                            <p className={'font-size-24 bold'}>
-                                {this.props.salary} руб/мес
-                            </p>
-                        ),
-                    },
-                    {
-                        header: 'Требуемый опыт работы',
-                        inside: (
-                            <p className={'font-size-24 bold'}>
-                                {this.props.experience}
-                            </p>
-                        ),
-                    },
-                    {
-                        header: 'Информация о вакансии',
-                        inside: (
-                            <div className={'flex column g-16'}>
-                                <RenderWithCondition
-                                    condition={!!this.props.location}
-                                    onSuccess={
-                                        <IconField
-                                            icon={MapIcon}
-                                            content={this.props.location}
-                                        />
-                                    }
+        const content = [];
+        if (this.props.salary && this.props.salary !== '') {
+            content.push({
+                header: 'Заработная плата',
+                inside: (
+                    <p className={'font-size-24 bold'}>
+                        {this.props.salary} руб/мес
+                    </p>
+                ),
+            });
+        }
+
+        if (this.props.experience && this.props.experience !== '') {
+            content.push({
+                header: 'Требуемый опыт работы',
+                inside: (
+                    <p className={'font-size-24 bold'}>
+                        {this.props.experience}
+                    </p>
+                ),
+            });
+        }
+
+        if (this.props.location || this.props.hours || this.props.format) {
+            content.push({
+                header: 'Информация о вакансии',
+                inside: (
+                    <div className={'flex column g-16'}>
+                        <RenderWithCondition
+                            condition={!!this.props.location}
+                            onSuccess={
+                                <IconField
+                                    icon={MapIcon}
+                                    content={this.props.location}
                                 />
-                                <RenderWithCondition
-                                    condition={!!this.props.hours}
-                                    onSuccess={
-                                        <IconField
-                                            icon={CalendarIcon}
-                                            content={this.props.hours}
-                                        />
-                                    }
+                            }
+                        />
+                        <RenderWithCondition
+                            condition={!!this.props.hours}
+                            onSuccess={
+                                <IconField
+                                    icon={CalendarIcon}
+                                    content={this.props.hours}
                                 />
-                                <RenderWithCondition
-                                    condition={!!this.props.format}
-                                    onSuccess={
-                                        <IconField
-                                            icon={ClockIcon}
-                                            content={this.props.format}
-                                        />
-                                    }
+                            }
+                        />
+                        <RenderWithCondition
+                            condition={!!this.props.format}
+                            onSuccess={
+                                <IconField
+                                    icon={ClockIcon}
+                                    content={this.props.format}
                                 />
-                            </div>
-                        ),
-                    },
-                    // {
-                    //     header: 'Профессиональные навыки',
-                    //     inside: (
-                    //         <div className={'flex row g-8 flex-wrap'}>
-                    //             {this.props.skills?.map(item => (
-                    //                 <Chips>{item}</Chips>
-                    //             ))}
-                    //         </div>
-                    //     ),
-                    // },
-                    // {
-                    //     header: 'Социальные сети',
-                    //     inside: (
-                    //         <div className={'flex row g-16'}>
-                    //             <div
-                    //                 className={'inner-svg-h-24 inner-svg-200'}
-                    //                 dangerouslySetInnerHTML={{
-                    //                     __html: VKIcon,
-                    //                 }}
-                    //             ></div>
-                    //             <div
-                    //                 className={'inner-svg-h-24 inner-svg-200'}
-                    //                 dangerouslySetInnerHTML={{
-                    //                     __html: FacebookIcon,
-                    //                 }}
-                    //             ></div>
-                    //             <div
-                    //                 className={'inner-svg-h-24 inner-svg-200'}
-                    //                 dangerouslySetInnerHTML={{
-                    //                     __html: TelegramIcon,
-                    //                 }}
-                    //             ></div>
-                    //         </div>
-                    //     ),
-                    // },
-                ]}
-            />
-        );
+                            }
+                        />
+                    </div>
+                ),
+            });
+        }
+
+        return <SideBar content={content} />;
     }
 }
 
