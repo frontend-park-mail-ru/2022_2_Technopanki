c8a8c5475b50e413d7ef5b5d44a904a5402fcc60
Vladislav <sdvlad75@gmail.com>
2022-11-11 17:51:28 +0300

    test

--- src/components/ProfileHeader/ProfileHeader.tsx
+++ src/components/ProfileHeader/ProfileHeader.tsx
@@ -16,7 +16,7 @@ export default class ProfileHeader extends Component<{
             <div className={`flex column ${styles.profile}`}>
                 <div key={'banner'} className={styles.profile_banner}></div>
                 <div key={'content'} className={styles.profile_content}>
-                    <div key={'info'} className={styles.info}>
+                    <div className={styles.info}>
                         <img
                             className={styles.info__avatar}
                             src={this.props.avatarSrc}
@@ -29,7 +29,7 @@ export default class ProfileHeader extends Component<{
                             <p key={'status'}>{this.props.status}</p>
                         </div>
                     </div>
-                    <div key={'buttons'}>{this.props.buttons}</div>
+                    <div>{this.props.buttons}</div>
                 </div>
             </div>
         );
