b99f1fa785eb2998a3fc9dcc54c94e52a8df5bd2
sonichka-s <stnchnk0@gmail.com>
2022-11-24 03:50:14 +0300

    fix(UI-kit): fixed search input

---
+++ src/components/UI-kit/forms/inputs/SearchDropdownMenu.tsx
@@ -0,0 +1,9 @@
+import { ReactsComponent } from '../../../../../Reacts';
+
+export default class DropdownMenu extends ReactsComponent{
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
--- src/components/UI-kit/forms/inputs/SearchInput.tsx
+++ src/components/UI-kit/forms/inputs/SearchInput.tsx
@@ -8,34 +8,36 @@ import ButtonSearchBlue from '../../buttons/ButtonSearchBlue';
 export default class SearchInput extends ReactsComponent {
     render() {
         return (
-            <form className={'flex column relative'}>
-                <input
-                    className={`w-100 ${styles.input} ${styles.search_input}`}
-                    type={'text'}
-                    placeholder={'Поиск...'}
-                />
-                <div className={`absolute flex row g-32 ${styles.menu}`}>
-                    <div className={'flex row g-8'}>
-                        <p className={`cursor-pointer ${styles.menu_data}`}>
-                            Вакансии
-                        </p>
+            <form className={'flex column relative screen-responsive'}>
+                <div className={'relative'}>
+                    <input
+                        className={`w-100 absolute ${styles.input}`}
+                        type={'text'}
+                        placeholder={'Поиск...'}
+                    />
+                    <div className={`absolute flex row ${styles.menu}`}>
+                        <div className={'flex row g-8'}>
+                            <p className={`cursor-pointer ${styles.menu_data}`}>
+                                Вакансии
+                            </p>
+                            <div
+                                className={`cursor-pointer ${styles.search_arrow}`}
+                                dangerouslySetInnerHTML={{ __html: SearchArrow }}
+                            />
+                        </div>
+                        <div className={`${styles.search_line} ${styles.menu_data}`}
+                             dangerouslySetInnerHTML={{ __html: Line }}
+                        />
                         <div
-                            className={`cursor-pointer ${styles.search_arrow}`}
-                            dangerouslySetInnerHTML={{ __html: SearchArrow }}
+                            className={`cursor-pointer ${styles.icon}`}
+                            dangerouslySetInnerHTML={{ __html: Loupe }}
                         />
                     </div>
-                    <div className={`${styles.search_line} ${styles.menu_data}`}
-                         dangerouslySetInnerHTML={{ __html: Line }}
-                    />
-                    <div
-                        className={`cursor-pointer ${styles.icon}`}
-                        dangerouslySetInnerHTML={{ __html: Loupe }}
-                    />
-                </div>
-                <div className={`absolute pt-4 r-0-4`}>
-                    <ButtonSearchBlue>
-                        Найти
-                    </ButtonSearchBlue>
+                    <div className={`absolute pt-4 r-0-4`}>
+                        <ButtonSearchBlue>
+                            Найти
+                        </ButtonSearchBlue>
+                    </div>
                 </div>
             </form>
         );
