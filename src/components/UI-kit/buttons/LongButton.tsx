f23aa86b42bcd66adb576f4fb979cc99f00317a0
sonichka-s <stnchnk0@gmail.com>
2022-12-16 22:06:14 +0300

    feat(UI-kit): added simple featured layout

--- src/components/UI-kit/buttons/LongButton.tsx
+++ src/components/UI-kit/buttons/LongButton.tsx
@@ -4,12 +4,20 @@ import styles from './button.module.scss'
 
 export default class LongButton extends ReactsComponent<{
     direction: 'right' | 'left';
-    content: ReactsComponentNode
+    content: ReactsComponentNode | string;
 }>{
     render() {
         return (
-            <div className={`flex row align-items-center justify-content-center ${styles.long_button}-${this.props.direction}`}>
-                {this.props.content}
+            <div className={'w-100'}>
+                {this.props.direction === 'right' ? (
+                    <div className={`flex row align-items-center justify-content-center w-100 ${styles.long_button_right}`}>
+                        {this.props.content}
+                    </div>
+                ) : (
+                    <div className={`flex row align-items-center justify-content-center w-100 ${styles.long_button_left}`}>
+                        {this.props.content}
+                    </div>
+                )}
             </div>
         )
     }
