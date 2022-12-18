73e0c4d77aec4a9973a891a4c8851f8eedc7445f
vladislav <sdvlad75@gmail.com>
2022-12-18 19:59:45 +0000

    added manifest.json

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,10 +1,11 @@
 const files = ['index.html'];
-const CACHE_NAME = 'jobflowPWA-v1';
 
 const CACHE = {
     STATIC: 'jobflow_static-v1',
     HTML: 'jobflow_html-v1',
     JS: 'jobflow_js-v1',
+    IMAGES: 'jobflow_image-v1',
+    SERVER_RESPONSES: 'jobflow_resp-v1',
 };
 
 // const CACHE = 'cache';
@@ -12,56 +13,100 @@ const CACHE = {
 self.addEventListener('install', event => {
     console.log('Установлен');
     event.waitUntil(async () => {
-        const cache = await caches.open(CACHE_NAME);
+        caches.open(CACHE.HTML).then(cache => cache.addAll([]));
+        caches.open(CACHE.JS).then(cache => cache.addAll([]));
+        caches.open(CACHE.STATIC).then(cache => cache.addAll([]));
+        caches.open(CACHE.IMAGES).then(cache => cache.addAll([]));
+        caches.open(CACHE.SERVER_RESPONSES).then(cache => cache.addAll([]));
     });
-    event.waitUntil(
-        caches.open(CACHE).then(cache => {
-            return cache.addAll([]);
-        }),
-    );
 });
 
 self.addEventListener('activate', event => {
     console.log('Активирован');
 });
 
-const fetchFromCache = event => {
-    return caches.open(CACHE_NAME).then(cache => {
-        cache.match(event).then(matched => {
-            if (!matched) {
-                throw new Error(`${event} not found in cache`);
+// const fetchFromCache = event => {
+//     return caches.open(CACHE_NAME).then(cache => {
+//         cache.match(event).then(matched => {
+//             if (!matched) {
+//                 throw new Error(`${event} not found in cache`);
+//             }
+
+//             return matched;
+//         });
+//     });
+// };
+
+const serveCache = (event, cacheName) => {
+    event.respondWith(
+        (async () => {
+            const request = await caches.match(event.request);
+            console.log(
+                `[Service Worker] Fetching resourse from: ${event.request.url}`,
+            );
+
+            // return cached response
+            if (request) {
+                return request;
             }
 
-            return matched;
-        });
-    });
+            // get response from server
+            const response = await fetch(event.request);
+            const cache = await caches.open(cacheName);
+
+            console.log(
+                `[Service Worker] Caching new resource: ${event.request.url}`,
+            );
+
+            // save response to cache
+            await cache.put(event.request, response.clone());
+            return response;
+        })(),
+    );
+};
+
+const getCacheName = url => {
+    if (/\.html$/.test(url)) {
+        return CACHE.HTML;
+    } else if (/.js$/.test(url)) {
+        return CACHE.JS;
+    } else if (/\^api\/user/.test(url)) {
+        return CACHE.SERVER_RESPONSES;
+    } else if (/.webp\??.*?$/.test(url)) {
+        return CACHE.IMAGES;
+    } else {
+        return CACHE.STATIC;
+    }
 };
 
 self.addEventListener('fetch', event => {
     console.log('Происходит запрос на сервер');
-    event.respondWith((async () => {
-        const request = await caches.match(event.request);
-        console.log(
-            `[Service Worker] Fetching resourse from: ${event.request.url}`,
-        );
+    event.respondWith(
+        (async () => {
+            try {
+                // get response from server
+                const response = await fetch(event.request);
+                const cache = await caches.open(
+                    getCacheName(event.request.url),
+                );
 
-        // return cached response
-        if (request) {
-            return request;
-        }
+                console.log(
+                    `[Service Worker] Caching new resource: ${event.request.url}`,
+                );
 
-        // get response from server
-        const response = await fetch(event.request);
-        const cache = await caches.open(CACHE_NAME);
-
-        console.log(
-            `[Service Worker] Caching new resource: ${event.request.url}`,
-        );
-
-        // save response to cache
-        await cache.put(event.request, response.clone());
-        return response;
-    })());
+                // save response to cache
+                await cache.put(event.request, response.clone());
+                return response;
+            } catch (e) {
+                const request = await caches.match(event.request);
+                
+                // return cached response
+                if (request) {
+                    return request;
+                }
+            }
+        })(),
+    );
 });
 
 // matched || Promise.reject('no-matched')
